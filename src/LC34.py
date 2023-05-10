0b6bc5ac23a47911db7dd34ed049ffccece4fadd
Hongrui Wu <whr812756608@gmail.com>
2023-05-09 23:45:24 -0500

    Create LC34.py

---
+++ src/LC34.py
@@ -0,0 +1,22 @@
+Class solution:
+  def searchRange(self, nums: List[int], target: int) -> List[int]:
+    
+    def lower_bound(nums:List[int], target: int) -> int:
+      left = 0
+      right = len(nums) - 1
+      
+      while left <= right:
+        mid = left + (right-left) //2
+        if nums[mid] < target:
+          left = mid + 1
+        else:
+          right = mid -1 
+      return left
+   
+  start = lower_bound(nums,target)
+  end = lower_bound(nums,target+1) -1
+  if start == len(nums) or nums[start] != target:
+    return [-1,1]
+  
+  return [start, end]
+      
