ad47ba14c4ce88f6426da8320f54821f2970eb3c
Hongrui Wu <whr812756608@gmail.com>
2023-05-09 22:51:44 -0500

    Create LC35.py

---
+++ src/binarysearch/LC35.py
@@ -0,0 +1,18 @@
+class solution:
+  def serachInsert(self, nums: List[int], target: int) -> int:
+    left = 0
+    right = len(nums) -1 
+    
+    while left <= right:
+      mid = left + (right - left) // 2
+      if nums[mid] < target:
+        left = mid + 1
+      elif nums[mid] > target:
+        right = mid - 1
+      else:
+        return mid 
+     return right + 1  # return left 也许， 
+     # 终止时 right 在 left 左边， right + 1 = left
+     # 插入到right 和 left 中间
+      
+      
