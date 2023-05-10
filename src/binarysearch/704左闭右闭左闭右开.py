ea263b93bc49960331840f8dc54f09212be53001
Hongrui Wu <whr812756608@gmail.com>
2023-05-09 21:35:01 -0500

    Update and rename 704_inclusive_left_right.py to 704左闭右闭左闭右开.py

--- src/binarysearch/704左闭右闭左闭右开.py
+++ src/binarysearch/704左闭右闭左闭右开.py
@@ -19,3 +19,25 @@ class Solution:
             else:                    # nums[mid] == target
                 return mid
         return -1
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # 左闭右开写法
+        left = 0  # left boundary (index 0)
+        right = len(nums) # 右区间改为 len（nums） 防止数组只有一个元素
+        
+        while left < right:  #左闭右开 left 不能等于 right 合法区间 【1，2）
+            mid = left + (right-left) //2  #防止left + right /2越界 
+
+            if nums[mid] > target:    # nums[mid] > target
+                right = mid      # 说明target 在 mid 左边，更新右端点
+                                    # 更新为 right = mid
+                                   #  右开区间，已知nums[mid] > target
+                                   #  需要包含mid                                 
+            elif nums[mid] < target: # nums[mid] < target
+                left = mid + 1       # 说明target 在 mid 右边
+                                     # 更新左端点，left = mid + 1
+                                     #  左闭区间不需要包含mid
+            else:                    # nums[mid] == target
+                return mid
+        return -1
