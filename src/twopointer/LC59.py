e49fd4ba9773c7e0dd38755bbd0f24530c04413e
Hongrui Wu <whr812756608@gmail.com>
2023-05-11 00:51:13 -0500

    Create LC59.py

---
+++ src/twopointer/LC59.py
@@ -0,0 +1,29 @@
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        matrix = [[0 for _ in range(n)] for _ in range(n)]
+        left = 0
+        right = n-1
+        top = 0
+        bottom = n-1
+
+        nums = 1
+        target = n*n
+
+        while nums <= target:
+            for j in range(left,right+1):
+                matrix[top][j] = nums
+                nums +=1
+            top +=1
+            for i in range(top,bottom+1):
+                matrix[i][right] = nums
+                nums +=1
+            right-=1
+            for j in range(right,left-1,-1):
+                matrix[bottom][j] = nums
+                nums +=1
+            bottom -=1
+            for i in range(bottom,top-1,-1):
+                matrix[i][left] = nums
+                nums+=1
+            left +=1
+        return matrix
