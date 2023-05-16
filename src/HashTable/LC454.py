fe409fbc566732c50ecf94b64d698868029758d8
Hongrui Wu <whr812756608@gmail.com>
2023-05-16 00:13:34 -0500

    Update LC454.py

--- src/HashTable/LC454.py
+++ src/HashTable/LC454.py
@@ -16,3 +16,23 @@ class Solution:
                                               # count += the value of hashmap[0-(c+d)]
                                               # the occurance time in ()a+b)
         return count
+    
+    # use dict(), frist need to check if the key already exist in hashmap,
+    # if not, create a key and set its value to 1
+    class Solution:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        hashmap = dict()
+        count = 0
+
+        for a in nums1:
+            for b in nums2:
+                if a+b in hashmap:
+                    hashmap[a+b] +=1
+                else:
+                    hashmap[a+b] =1
+        
+        for c in nums3:
+            for d in nums4:
+                if 0 - (c+d) in hashmap:
+                    count += hashmap[0-(c+d)]
+        return count
