aee65378fec7a30fab31a7ccb601f9023f8518c6
Hongrui Wu <whr812756608@gmail.com>
2023-05-17 17:15:46 -0500

    Create LC541.py

---
+++ src/twopointer/LC541.py
@@ -0,0 +1,22 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        def reverse(s):
+            left = 0
+            right = len(s)-1
+            while left < right:
+                s[left],s[right] = s[right],s[left]
+                left  +=1
+                right -=1
+            return s
+        s= list(s)
+
+        print(s[::-1])
+
+        for i in range(0,len(s),2*k):
+            if i + k <= len(s):              # check between i+k and len(s)
+                                             # exclusive len(s), so we can write <=
+                                             # if its inclusive, can't write =
+                s[i:i+k] = reverse(s[i:i+k]) # modify on the slice
+            else:
+                s[i:] = reverse(s[i:])
+        return "".join(s)
