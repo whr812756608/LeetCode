16fb8251a86cd144b9afc97b13505806834f0608
Hongrui Wu <whr812756608@gmail.com>
2023-05-18 00:08:13 -0500

    Update reverseLeftWords.py

--- src/twopointer/reverseLeftWords.py
+++ src/twopointer/reverseLeftWords.py
@@ -27,3 +27,12 @@ class Solution:
         
         # space complexity O(N)
         # return  s[n:] + s[:n] 
+        
+        #取余法：
+   class Solution:
+    def reverseLeftWords(self, s: str, n: int) -> str:
+        res = []
+        for i in range(n,n+len(s)):       # 取余数，i%len（s）= i，python可以直接+
+           res += s[i%len(s)]             # 当 i 小于 len（s）时，i=3，i=4，i%len（s）= i，余数为i，即对应下标，res【3】=s【3】，res【4】=s【4】
+           print(res)                     # 当 i 大于 len（s）时，i =7，i=8
+        return "".join(res)               # i%len（s）= i - len（s） 余数为 下标-长度，即res【7】=s【1】，res【8】=s【2】
