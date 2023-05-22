d8a8fb4021109efc109e47e88bed428bab6e680b
Hongrui Wu <whr812756608@gmail.com>
2023-05-22 13:18:54 -0500

    Create LC20.py

---
+++ src/stack_deque/LC20.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for c in s:
+            if c == "{":
+                stack.append("}")
+            elif c == "[":
+                stack.append("]")
+            elif c == "(":
+                stack.append(")")
+            elif not stack or c != stack[-1]:
+                return False 
+            else:
+                stack.pop()
+        return True if not stack else False
