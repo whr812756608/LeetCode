2849e100181a261b6841c93e36af0446464edfed
Hongrui Wu <whr812756608@gmail.com>
2023-05-22 13:21:35 -0500

    Update LC20.py

--- src/stack_deque/LC20.py
+++ src/stack_deque/LC20.py
@@ -8,7 +8,7 @@ class Solution:
                 stack.append("]")
             elif c == "(":
                 stack.append(")")
-            elif not stack or c != stack[-1]:
+            elif not stack or c != stack[-1]:  #重点条件，not stack 指右括号多（左右pop完了，新来一个右括号），c！=stack 指左右括号不匹配
                 return False 
             else:
                 stack.pop()
